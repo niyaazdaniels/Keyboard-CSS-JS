da9668ee37ddb69933711d1c7865161e934347c1
Niyaaz Daniels <niyaazdaniels1@gmail.com>
2023-11-06 15:40:13 +0200

    something

---
+++ index.js
@@ -0,0 +1,35 @@
+// function myFunction() {
+//     alert ('I am the king of JavaScript!');
+// }
+
+//onkeyup occurs when the user releases a key on the keyboard.
+
+//function displays value
+function dis(val){
+    document.getElementById("result").value += val
+} 
+
+function myFunction(event) {
+    if (event.key == '0' || event.key =='1'
+        || event.key == '2' || event.key == '3'
+        || event.key == '4' || event.key == '5'
+        || event.key == '6' || event.key == '7'
+        || event.key == '8' || event.key == '9'
+        || event.key == '+' || event.key == '-'
+        || event.key == '*' || event.key == '/' )
+        document.getElementById("result").value += event.key;
+}
+
+var cal = document.getElementById("calcu");
+cal.onkeyup = function (event) {
+    if (event.keyCode === 13){
+        console.log("Enter");
+        let x = document.getElementById("result").value
+        console.log(x);
+        solve();
+    }
+}
+// function that clear the display 
+function clr () {
+    document.getElementById("result").value = ""
+}
